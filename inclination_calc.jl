d2ce40017be17774cc09e065dbb6fdbcf13ec565
Lisa Bacherle <erlenbach19@gmail.com>
2022-04-06 15:28:30 +0000

    added azimuth calculation

--- inclination_calc.jl
+++ inclination_calc.jl
@@ -5,7 +5,7 @@ function parse_value(lower, upper, name)
         try
             value = parse(typeof(lower), value)
             if (value > upper) || (value < lower)
-                println("That number was out of bounds :/  It should be between $lower and $upper (duh). Try again!")
+                println("That number was out of bounds :/  The value has to be between $lower and $upper. Try again!")
                 value = chomp(readline())
             else
                 break
@@ -15,7 +15,7 @@ function parse_value(lower, upper, name)
             value = chomp(readline())
         end
     end
-    println("Cool, your chosen $name is $value")
+    println("Your chosen $name is $value")
     return value
 end
 
@@ -27,16 +27,19 @@ function calc_inc_given_latitude()
     calc_inc(lat, az)
 end
 
-function calc_inc_given_launchsite(launchsites)
+function choose_launchsite_and_get_latitude(launchsites)
     names = launchsites.names
     lats = launchsites.latitudes
     run(`clear`) 
     println("You chose calculating with a given launchsite. Great choice, if I may say so!")
-    println("You have the following choices:\n $names \n Please type 1 for the first option, 2 for the second...you get it!")
+    println("Please choose your launchsite now.")
+    println("You have the following choices:\n $names \n Type 1 for the first option, 2 for the second...")
     launchsite = parse_value(1, length(lats), "launchsite")
     lat = lats[launchsite]
-    println(lats)
-    println(lat)
+    return lat
+end
+function calc_inc_given_launchsite(launchsites)
+    lat = choose_launchsite_and_get_latitude(launchsites)
     az = parse_value(0.0, 360.0, "azimuth")
     calc_inc(lat, az)
 end
@@ -47,10 +50,49 @@ function calc_inc(lat, az)
     return inc
 end
 function calc_az_given_launchsite(launchsites)
-
+    while(true)
+        lat = choose_launchsite_and_get_latitude(launchsites)
+        inc = parse_value(-90.0,90.0,"inclination")
+        if(inc<lat)
+            println("The inclination must be greater than or equal to the launch latitude, otherwise there is no mathematical solution to this problem. Please try entering different values.")
+        else
+            break
+        end
+    end
+    az = calc_az(lat, inc)
+    return az
+end
+function calc_az_given_latitude()
+    lat = parse_value(-90.0, 90.0, "latitude")
+    inc = parse_value(-90.0,90.0,"inclination")
+    while(true)
+        if(inc<lat)
+            println("The inclination must be greater than or equal to the launch latitude, otherwise there is no mathematical solution to this problem. Please try entering different values.")
+        else
+            break
+        end
+    end
+    az = calc_az(lat, inc)
+    return az
+end
+function calc_az(lat, inc)
+    println(".....calculating azimuth......")
+    az = asind(cosd(inc)/cosd(lat))
+    if inc!=lat
+        if (cosd(inc)/cosd(lat))>0
+            az2 =180- asind(cosd(inc)/cosd(lat))
+            println("The problem has two possible solutions, the first Azimuth is $az, the second one is $az2")
+        else
+            az2 = -180-asind(cosd(inc)/cosd(lat))
+            println("The problem has two possible solutions, the first Azimuth is $az, the second one is $az2")
+        end
+    else 
+        println("There is only one possible Azimuth that results in the given inclination, which is $az")
+    end
+    return az
 end
 function latitude_or_launchsite()
-    println("Do you want to provide your own latitude or choose from a given launch site? Type 1 for latitude, 2 for launch site then press enter.")
+    println("Do you want to provide your own latitude or choose from a given launch site? Type 1 for latitude, 2 for launch site.")
     choice = parse_value(1, 2, "mode")
     if choice == 1
         own_lat = true
@@ -62,7 +104,7 @@ function latitude_or_launchsite()
     return own_lat
 end
 function choose_mode(launchsites)
-    println("Do you want to calculate inclination or Azimuth? Type 1 for inclination, 2 for Azimuth")
+    println("Do you want to calculate inclination or Azimuth? Type 1 for inclination, 2 for Azimuth then press enter")
     choice = parse_value(1, 2, "mode")
     #calculate inclination for given azimuth 
     if choice == 1
@@ -76,10 +118,9 @@ function choose_mode(launchsites)
     elseif choice == 2
         own_lat = latitude_or_launchsite()
         if own_lat
-            #calc azimuth for a given latitude
+            calc_az_given_latitude()
         else
             calc_az_given_launchsite(launchsites)
-            #calc azimuth for a given launchsite
         end
     else
         throw(BoundsError(choice,"whoa, that argument was out of bounds. how did you do that?"))
