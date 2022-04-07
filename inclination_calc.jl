0adcb462264a40ff369472c915ba38be13e11dea
Lisa Bacherle <erlenbach19@gmail.com>
2022-04-07 17:43:02 +0000

    added rotating launch azimuth calculation

--- inclination_calc.jl
+++ inclination_calc.jl
@@ -56,38 +56,93 @@ function calc_az_given_launchsite(launchsites)
     lat = choose_launchsite_and_get_latitude(launchsites)
     inc = parse_value(-90.0,90.0,"inclination")
     while(true)
-        if(inc<lat)
+        if abs(inc)<lat
             println("The inclination must be greater than or equal to the launch latitude, otherwise there is no mathematical solution to this problem. Please try entering different values.")
+            lat = choose_launchsite_and_get_latitude(launchsites)
+            inc = parse_value(-90.0,90.0,"inclination")
         else
             break
         end
     end
-    az = calc_az(lat, inc)
+    az = calc_az(lat, inc, true)
     return az
+end
+
+function calc_rotational_az_given_launchsite(launchsites)
+    run(`clear`)
+    print("hi")
+    lat = choose_launchsite_and_get_latitude(launchsites)
+    inc = parse_value(-90.0,90.0,"inclination")
+    v = parse_value(0.0,typemax(Float32),"orbit speed")
+    while(true)
+        if abs(inc)<lat
+            println("The inclination must be greater than or equal to the launch latitude, otherwise there is no mathematical solution to this problem. Please try entering different values.")
+            lat = parse_value(-90.0, 90.0, "latitude")
+            inc = parse_value(-90.0,90.0,"inclination")
+            v = parse_value(0.0,typemax(Float32),"orbit speed")
+        else
+            break
+        end
+    end
+    az = calc_rotational_az(lat, inc,v)
+    return az
+end
+function calc_rotational_az_given_latitude()
+    lat = parse_value(-90.0, 90.0, "latitude")
+    inc = parse_value(-90.0,90.0,"inclination")
+    v = parse_value(0.0,typemax(Float32),"orbit speed")
+    while(true)
+        if abs(inc)<lat 
+            println("The inclination must be greater than or equal to the launch latitude, otherwise there is no mathematical solution to this problem. Please try entering different values.")
+            lat = parse_value(-90.0, 90.0, "latitude")
+            inc = parse_value(-90.0,90.0,"inclination")
+            v = parse_value(0.0,typemax(Float32),"orbit speed")
+        else
+            break
+        end
+    end
+    az = calc_rotational_az(lat, inc, v)
+    return az
+end
+function calc_rotational_az(lat, inc, v)
+    println(".....calculating azimuth......")
+    az_inertial = calc_az(lat, inc, false)
+    v_eqrot = 465.101
+    az_rot = atand((v * sind(az_inertial) - v_eqrot*cosd(inc))/ (v*cosd(az_inertial)))
+    println("The rotational Azimuth needed to achieve the desired inclination of $inc ° is $az_rot °")
+    return az_rot
 end
 function calc_az_given_latitude()
     lat = parse_value(-90.0, 90.0, "latitude")
     inc = parse_value(-90.0,90.0,"inclination")
     while(true)
-        if inc<lat 
+        if abs(inc)<lat 
             println("The inclination must be greater than or equal to the launch latitude, otherwise there is no mathematical solution to this problem. Please try entering different values.")
+            lat = parse_value(-90.0, 90.0, "latitude")
+            inc = parse_value(-90.0,90.0,"inclination")
         else
             break
         end
     end
-    az = calc_az(lat, inc)
+    az = calc_az(lat, inc,true)
     return az
 end
-function calc_az(lat, inc)
-    println(".....calculating azimuth......")
+function calc_az(lat, inc, verbose)
+    if verbose
+        println(".....calculating azimuth......")
+    end
     az = asind(cosd(inc)/cosd(lat))
     if inc!=lat
         if (cosd(inc)/cosd(lat))>0
             az2 =180- asind(cosd(inc)/cosd(lat))
-            println("The problem has two possible solutions, the first Azimuth is $az, the second one is $az2")
+            if verbose
+                println("The problem has two possible solutions, the first Azimuth is $az, the second one is $az2")
+            end
         else
             az2 = -180-asind(cosd(inc)/cosd(lat))
-            println("The problem has two possible solutions, the first Azimuth is $az, the second one is $az2")
+            if verbose
+                println("The problem has two possible solutions, the first Azimuth is $az, the second one is $az2")
+            end
         end
     else 
         println("There is only one possible Azimuth that results in the given inclination, which is $az")
@@ -107,35 +162,46 @@ function latitude_or_launchsite()
     end
     return own_lat
 end
+
 function choose_mode(launchsites)
     run(`clear`)
     println("Do you want your calculations to take into account the rotation of the earth? To include rotation, target orbit speed must be known. Type 1 for yes, 2 for no")
     choice = parse_value(1, 2, "mode")
+    rot = true
     if choice == 1
         rot = true
-    else
-        rot = false
     end
-    println("Do you want to calculate inclination or Azimuth? Type 1 for inclination, 2 for Azimuth then press enter")
-    choice = parse_value(1, 2, "mode")
-    #calculate inclination for given azimuth 
-    if choice == 1
-        own_lat = latitude_or_launchsite()
-        if own_lat
-            calc_inc_given_latitude()
+    if !rot
+        println("Do you want to calculate inclination or Azimuth? Type 1 for inclination, 2 for Azimuth then press enter")
+        choice = parse_value(1, 2, "mode")
+        #calculate inclination for given azimuth 
+        if choice == 1
+            own_lat = latitude_or_launchsite()
+            if own_lat
+                calc_inc_given_latitude()
+            else
+                calc_inc_given_launchsite(launchsites)
+            end
+        #calculate azimuth for given inclination and latitude
+        elseif choice == 2
+            own_lat = latitude_or_launchsite()
+            if own_lat
+                calc_az_given_latitude()
+            else
+                calc_az_given_launchsite(launchsites)
+            end
         else
-            calc_inc_given_launchsite(launchsites)
-        end
-    #calculate azimuth for given inclination and latitude
-    elseif choice == 2
-        own_lat = latitude_or_launchsite()
-        if own_lat
-            calc_az_given_latitude()
-        else
-            calc_az_given_launchsite(launchsites)
+            throw(BoundsError(choice,"whoa, that argument was out of bounds. how did you do that?"))
         end
     else
-        throw(BoundsError(choice,"whoa, that argument was out of bounds. how did you do that?"))
+        println("In rotational mode you can only calculate Azimuth for a desired inclination")
+        own_lat = latitude_or_launchsite()
+        print("hi")
+        if own_lat
+            calc_rotational_az_given_latitude()
+        else
+            calc_rotational_az_given_launchsite(launchsites)
+        end
     end
 end
 struct Launch_sites
