ebc64540ab5a1d8ef2721d8443a2f28868492b69
Lisa Bacherle <erlenbach19@gmail.com>
2022-04-18 21:37:54 +0000

    updated header comment

--- inclination_calc.jl
+++ inclination_calc.jl
@@ -31,7 +31,7 @@ Dependencies:
         julia> Pkg.add("CSV")
 
 Sources:
-    1) Formulas: The derivation of formulas for inertial and rotational calculations can be found under https://www.orbiterwiki.org/index.php?title=Launch_Azimuth&oldid=17141
+    1) Formulas and parameters: The derivation of formulas for inertial and rotational calculations can be found under https://www.orbiterwiki.org/index.php?title=Launch_Azimuth&oldid=17141
                  or https://ntrs.nasa.gov/api/citations/19980227091/downloads/19980227091.pdf ('Technical Note D-233, Determination of Azimuth Angle at Burnout for placing Satellite over a selected Earth position', T.H. Skopinski and K.G Johnson, NASA, 1960)
     2) Launchsite Latitudes: 'Space Mission Engineering - The New SMAD' by Wertz, Everett and Puschell, published 2011 by Microcosm Press
 
